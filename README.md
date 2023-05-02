c88f191b854a6a1014d6784c4dfd19d3c86636e5
AriLaurin <arijuslaurin@gmail.com>
2023-05-02 10:57:00 +0200

    Imported Node & EJS template

--- README.md
+++
@@ -1,2 +0,0 @@
-# bloggenMin
-I dette prosjektet bruker jeg en Linux webserver for å hoste nettsiden. Dette kan jeg gjøre med bruk av Node og EJS. Grunnen for at jeg har valgt dette er fordi at jeg er veldig godt kjent med oppsettet av en Ubuntu Webserver og kan Browser Requests med Node og EJS.
---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const Routes = require("./routes/authRoutes"); //importing the routes so our app actually uses them
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json()); //takes any json data from requests, and parses it into a js code
+
+// view engine
+app.set('view engine', 'ejs');
+
+const dbURI = process.env.host
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3003))
+  .then(console.log("DATABASE STATUS: CONNECTED"))
+  .catch((err) => console.log(err));
+
+// routes
+app.use(Routes);
